710c237566a1d6a31e612f5f0d955a3c982b225d
abirdeyalexthehawk <abir.dey@agrud.com>
2023-03-08 09:42:07 +0530

    redux persist implementation pt 1

--- src/store.js
+++ src/store.js
@@ -1,7 +1,28 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import { CookieStorage } from "redux-persist-cookie-storage";
 import userDetailReducer from "./features/user/userDetail";
-export default configureStore({
+
+const persistConfig = {
+  key: "root",
+  storage: new CookieStorage({
+    expiration: {
+      // Optional - the cookie will expire after 1 hour by default
+      default: 60 * 60 * 1000,
+    },
+    serialize: (data) => JSON.stringify(data),
+    deserialize: (data) => JSON.parse(data),
+  }),
+  whitelist: ["user"], // Only persist the 'user' key
+};
+const persistedReducer = persistReducer(persistConfig, userDetailReducer);
+
+const store = configureStore({
   reducer: {
-    userDetail: userDetailReducer,
+    userDetail: persistedReducer,
   },
 });
+
+const persistor = persistStore(store);
+
+export { store, persistor };
